a33310eede7a7d5cde9b0a4e5682be266ebb80b9
Natecgg <natecgg@sfr.fr>
2019-06-15 16:46:04 +0200

    Nate : j'ai ajouté la librairie taglib# au projet, elle fonctionne. J'ai mis du code commenté dans le fichier filePath.cs (j'ai choisi ce fichier au hasard).  Avec Directory.GetFiles je peux mettre dans un string le nom de tous les fichiers mp3 contenus dans le dossier dont le chemin est communiqué en paramètre. Je me remets sur le projet demain.

--- Democrazik/filePath.cs
+++ Democrazik/filePath.cs
@@ -9,5 +9,17 @@ namespace Democrazik
     class filePath
     {
        public string adresse;
+
+        //         string path = "C:\Users\Nathael\Documents\";
+        //         string ext = "*.mp3";
+        //       string[] listFilesCurrDir = Directory.GetFiles(@"C:\Users\Nathael\Documents\", ext);
+
+        // string pathOfFile = @"C:\Users\Nathael\Desktop\tel\SD\Sons\Booba - Nouvelle Ecole.mp3";
+        //var tfile = TagLib.File.Create(@pathToPlay);
+        //string title = tfile.Tag.Title;
+
+        // string pathToPlay;
+        // axWindowsMediaPlayer1.URL = pathToPlay;
+
     }
 }
